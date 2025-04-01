26a97cf3866372ba5e7b28f81c56ff87952acdd6
MattRushmer <166296750+MattRushmer@users.noreply.github.com>
2025-04-01 14:27:03 +1300

    Adding Dark and light mode

--- Script.js
+++ Script.js
@@ -6,3 +6,153 @@ function a_open() {
 function a_close() {
     document.getElementById("mySidebar").style.display = "none";
 }
+
+// adds a preloader
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function () {
+    loader.style.display = "none";
+})
+
+
+
+
+
+// moves my top nav
+const topnav = document.querySelector(".topnav");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+    if (lastScrollY < window.scrollY) {
+        topnav.classList.add("topnav--hidden");
+    } else {
+        topnav.classList.remove("topnav--hidden");
+    }
+
+    lastScrollY = window.scrollY;
+});
+
+
+
+
+
+
+
+
+// check for saved 'darkMode' in localStorage
+let darkMode = localStorage.getItem('darkMode');
+let isDark = localStorage.getItem('dark');
+
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+const enableDarkMode = () => {
+    // 1. Add the class to the body
+    document.body.classList.add('darkmode');
+    // 2. Update darkMode in localStorage
+
+    document.documentElement.style.overflow = "hidden";
+    // trigger reflow so that overflow style is applied
+    document.body.clientWidth;
+    // change scheme
+    document.documentElement.setAttribute(
+        "data-color-scheme",
+        isDark ? "dark" : "light"
+
+
+    );
+
+    // remove overflow style, which will bring back the scrollbar with the correct scheme 
+    document.documentElement.style.overflow = "";
+    isDark = !isDark;
+
+    localStorage.setItem('darkMode', 'enabled');
+}
+
+const disableDarkMode = () => {
+    // 1. Remove the class from the body
+    document.body.classList.remove('darkmode');
+    // 2. Update darkMode in localStorage
+
+    document.documentElement.style.overflow = "hidden";
+    // trigger reflow so that overflow style is applied
+    document.body.clientWidth;
+    // change scheme
+    document.documentElement.setAttribute(
+        "data-color-scheme",
+        isDark ? "dark" : "light"
+
+
+    );
+
+    // remove overflow style, which will bring back the scrollbar with the correct scheme 
+    document.documentElement.style.overflow = "";
+    isDark = !isDark;
+
+    localStorage.setItem('darkMode', null);
+}
+
+// If the user already visited and enabled darkMode
+// start things off with it on
+if (darkMode === 'enabled') {
+    enableDarkMode();
+}
+
+// When someone clicks the button
+darkModeToggle.addEventListener('click', () => {
+
+
+    // get their darkMode setting
+    darkMode = localStorage.getItem('darkMode');
+
+    // if it not current enabled, enable it
+    if (darkMode !== 'enabled') {
+        enableDarkMode();
+        // if it has been enabled, turn it off  
+    } else {
+        disableDarkMode();
+    }
+});
+
+
+
+//scroll to top
+const btnScrollToTop = document.querySelector("#btnScrollToTop");
+
+btnScrollToTop.addEventListener("click", function () {
+
+    $("html,body").animate({ scrollTop: 0 }, "slow");
+});
+
+
+
+
+
+//show on scroll
+const hiddenElements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classlist.remove('show');
+        }
+    });
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
+
+const hiddenElements2 = document.querySelectorAll(".fade-in2");
+
+//show scroll 2
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classlist.remove('show');
+        }
+    });
+});
+
+hiddenElements2.forEach((el) => observer2.observe(el));
